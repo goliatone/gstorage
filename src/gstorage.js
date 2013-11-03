93778cbca82499ab06c4143bda699d97c9c8bddf
goliatone <burgosemi@gmail.com>
2013-11-02 20:46:52 -0400

    gstorage, initial commit. Implemented NullStorage and LocalStorage.

--- src/gstorage.js
+++ src/gstorage.js
@@ -34,14 +34,13 @@
     }
 
 }(this, "GStorage", ['jquery'], function($) {
-console.log('hjere', $);
 
 ///////////////////////////////////////////////////
 // CONSTRUCTOR
 ///////////////////////////////////////////////////
 	
 	var options = {
-        
+        purgeKey : '_g#$_'
     };
     
     /**
@@ -51,22 +50,132 @@ console.log('hjere', $);
      */
     var GStorage = function(config){
         $.extend(this, options, config || {});
-        this.options = options;   
-        this.init();  
+        this.options = options;
+        this.init();
     };
 
-    var o = $({});
-    GStorage.prototype.on   = function() { o.on.apply(o, arguments); return this;};
-    GStorage.prototype.off  = function() { o.off.apply(o, arguments); return this;};
-    GStorage.prototype.emit = function() { o.trigger.apply(o, arguments); return this;};
+    var o = $({}), _l = {};
+    var _a = function(e){ (e in _l) || (_l[e] = 0); _l[e] += 1; };
+    var _d = function(e){ (e in _l) && (_l[e] -= 1); };
+
+    GStorage.prototype.on    = function() { o.on.apply(o,  arguments); _a(arguments[0]); return this;};
+    GStorage.prototype.off   = function() { o.off.apply(o, arguments); _d(arguments[0]); return this;};
+    GStorage.prototype.emit  = function() { o.trigger.apply(o, arguments); return this;};
+    GStorage.prototype.emits = function(e){ return (e in _l && _l[e] > 0);};
 
 ///////////////////////////////////////////////////
-// PRIVATE METHODS
+// PUBLIC METHODS
 ///////////////////////////////////////////////////
 
     GStorage.prototype.init = function(){
-        console.log('GStorage: Init!');
-        return 'This is just a stub!';
+        this.store = new LocalStorage();
+        this.store.init(this.options);
     };
+
+    GStorage.prototype.use = function(ext){
+        $.extend(GConfig.prototype, ext);
+        return this;
+    };
+
+    GStorage.prototype.size = function(){
+        return this.store.size();
+    };
+
+    GStorage.prototype.get = function(key, def){
+        return this.store.get(key, def);
+    };
+
+    GStorage.prototype.set = function(key, value){
+        var old = this.store.get(key, null);
+        this.store.set(key, value);
+        //TODO: if(this.willtrigger('change'))
+        this.emit('change', key, old, value);
+        return this;
+    };
+
+    GStorage.prototype.del = function(key){
+        var old = this.store.get(key, null);
+        this.store.del(key);
+        this.emit('change', key, old);
+        return this;
+    };
+
+    GStorage.prototype.has = function(key){
+        return this.store.has(key);
+    };
+
+    GStorage.prototype.purge = function(){
+        this.store.purge();
+        return this;
+    };
+
+    GStorage.prototype.clear = function(){
+        this.store.clear();
+        return this;
+    };
+
+    GStorage.prototype.setTTL = function(key, time){
+
+    };
+
+    var NullStorage = function(){};
+    var methods = ['init', 'supported', 'size', 'get', 'set', 'del', 'has', 'purge', 'clear'];
+    for(var p in methods) NullStorage.prototype[methods[p]] = function(){};
+    
+    var LocalStorage = function(config){
+        $.extend(this, config || {});
+    };
+
+    LocalStorage.prototype.init = function(config){
+        $.extend(this, config || {});
+    };
+
+    LocalStorage.prototype.supported = function(){
+        var success = true;
+        try {
+            var value = Math.random();
+            storage.setItem(value, value);
+            storage.removeItem(value);
+        } catch (e) {
+            success = false;
+        }
+        
+        return success;
+    };
+
+    LocalStorage.prototype.size = function(){
+        return localStorage.length;
+    };
+
+    LocalStorage.prototype.get = function(key, def){
+        // 'undefined' === typeof def && def=null;
+        return this.has(key) ? localStorage.getItem(key) : def;
+    };
+
+    LocalStorage.prototype.set = function(key, value){
+        localStorage.setItem( key , value );
+    };
+
+    LocalStorage.prototype.del = function(key){
+        localStorage.removeItem(key);
+    };
+
+    LocalStorage.prototype.has = function(key){
+        return localStorage.getItem(key) !== null;
+    };
+
+    LocalStorage.prototype.purge = function(){
+        for ( var i = localStorage.length - 1; i >= 0; i-- ) {
+            if ( localStorage.key(i).indexOf(this.purgeKey) !== -1 ) {
+                localStorage.removeItem(localStorage.key(i));
+            }
+        }
+    };
+
+    LocalStorage.prototype.clear = function(){
+        localStorage.clear();
+    };
+
+
     return GStorage;
 }));
