421d52bfb27d6f13aa99f0d2b0ef90d09cf360d6
goliatone <burgosemi@gmail.com>
2014-09-22 16:53:06 -0400

    EXAMPLE: WIP...

--- examples/main.js
+++ examples/main.js
@@ -2,19 +2,55 @@
 /* jshint strict: false */
 requirejs.config({
     paths: {
-        'jquery': '../jquery/jquery',
-        'gstorage': '../gstorage'
+        'jquery': 'jquery/jquery',
+        'extend': 'gextend/extend',
+        'gpub': 'gpub/gpub',
+        'gstorage': 'gstorage',
+        'localstore': 'local.store',
+        'websqlstore': 'websql.store',
+        'indexedstore': 'indexed.store'
     }
 });
 
-define(['gstorage', 'jquery'], function (GStorage, $) {
+define('main', function(require) {
     console.log('Loading');
-	var gs = new GStorage();
-	// console.log(gs.has('test2'));
-	
-	gs.set('test2', 'something');
-	// console.log(gs.has('test2'));
-	// if(gs.has('test')) gs.get('test');
-	gs.get('test2');
-	window.gs = gs;
+    var GStorage = require('gstorage'),
+        GPup = require('gpub'),
+        LocalStore = require('localstore'),
+        WSQLStore = require('websqlstore'),
+        IndexedStore = require('indexedstore'),
+        $ = require('jquery');
+
+    GPup.observable(GStorage);
+    GPup.observable(LocalStore);
+    GPup.observable(WSQLStore);
+    GPup.observable(IndexedStore);
+
+    var gstorage = new GStorage({
+        buildDefaultStore: function() {
+            //Use NullStore
+            /*return new WSQLStore({
+                storeID: this.storeID,
+                storeName: this.storeName,
+                hashLength: this.hashLength
+            });*/
+            return new IndexedStore();
+            return new LocalStore();
+        }
+    });
+
+    window.gstorage = gstorage;
+
+
+    // var user = gstorage.get('user', false);
+    // if (!user) {
+    gstorage.on('connected', function() {
+        console.log('peperone')
+        this.get('user').on('success', function(e) {
+            console.log('==> Success', e);
+        });
+    });
+
+    // } else console.log(user)
+    // window.user = user;
 });
