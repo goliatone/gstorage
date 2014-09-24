a6fa60be4cfc6c0357b3a3a337c289be02a620d1
goliatone <burgosemi@gmail.com>
2014-09-23 22:22:21 -0400

    EXAMPLE: Updating to use PromisedDB layer.

--- examples/main.js
+++ examples/main.js
@@ -43,64 +43,64 @@ define('main', function(require) {
 
     var User = {
         add: function(person) {
-            return db.use('People', function(execute) {
+            return db.with('People', function(execute) {
                 this.objectStore('People')
                     .put(person);
             });
         },
         get: function(id) {
-            return db.use('People', function(execute) {
-                execute('user', this.objectStore('People')
+            return db.with('People', function(execute) {
+                execute(this.objectStore('People')
                     .get(id));
             });
         },
         del: function(id) {
-            return db.use('People', function(execute) {
+            return db.with('People', function(execute) {
                 this.objectStore('People')
                     .delete(id);
+            });
+        },
+        getUserStatus: function(id) {
+            var q = db.with('People', function(execute) {
+                execute(this.objectStore('People').get(id), 'user');
+            });
+
+            q.then(function(data) {
+                var user = data.user,
+                    active = user.active ? 'very active' : 'not very active';
+                console.debug(user.name + ' is ' + active);
+            }).catch(function(e) {
+                console.error('ERROR:', e);
             });
         }
     };
 
-
-    function getUserSilliness(id) {
-        var q = db.use('People', function(execute) {
-            execute('user', this.objectStore('People').get(id));
-        });
-
-        q.then(function(data) {
-            var user = data.user,
-                silliness = user.silly ? 'very silly' : 'not very silly';
-            console.log('HERE', user);
-            console.log(user.name + ' is ' + silliness);
-        }).catch(function(e) {
-            console.warn('WE DID HIT SOMETHING HERE', e)
-        });
-    }
-
     User.add({
         name: 'Pepe',
-        silly: false
+        active: false
     });
 
     var o = User.get(2);
     o.then(function(u) {
-        console.warn('GET USER THEN', u)
+        console.warn('=> GET USER RESULT:', u);
+        return u.result;
+    }).then(function(e){
+        console.warn('=> GET USER TRANSFORMED:', e);
     });
 
     User.add({
         name: 'Rone',
-        silly: true
+        active: true
     });
     User.add({
-        name: 'Zack',
-        silly: true
+        name: 'Goliat',
+        active: true
     });
 
     // User.del(1);
 
-    // 1 is the first ID that indexedDB registers.
-    getUserSilliness(1);
+    User.getUserStatus(1);
+
     return;
 
     var GStorage = require('gstorage'),
